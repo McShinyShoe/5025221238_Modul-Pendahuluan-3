5b2580d1d6c9586cd67cca3199e0d227a1ab8826
ShinyShoe <mcshinyshoe@gmail.com>
2023-11-23 10:30:06 +0700

    fix: 10th querry doesnt consider jumlah_minuman

--- 10.sql
+++ 10.sql
@@ -1,7 +1,7 @@
 -- 10
-SELECT nama_customer, nama_minuman, COUNT(*) FROM transaksi_minuman
+SELECT nama_customer, nama_minuman, SUM(jumlah_minuman) FROM transaksi_minuman
 JOIN transaksi ON id_transaksi = tm_transaksi_id
 JOIN customer ON customer_id_customer = id_customer
-JOIN menu_minuman ON tm_menu_minuman_id = id_minuman
+JOIN menu_minuw wman ON tm_menu_minuman_id = id_minuman
 WHERE nama_customer LIKE 'S%'
 GROUP BY nama_customer, nama_minuman
